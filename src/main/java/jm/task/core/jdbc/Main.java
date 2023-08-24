7653e05a3cfb945c81037f7981982f989a4b2de6
Nadezhda <anikeeva.nd@yandex.ru>
2023-08-25 02:34:22 +0400

    Практическая задача 1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,21 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        userService.createUsersTable();
+        userService.saveUser("Петя", "Один", (byte) 78);
+        userService.saveUser("Вася", "Двойко", (byte) 74);
+        userService.saveUser("Света", "Трояк", (byte) 59);
+        userService.saveUser("Катя", "Четверышка", (byte) 74);
+        userService.removeUserById(2);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
